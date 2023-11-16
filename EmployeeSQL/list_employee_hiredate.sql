523e05cf92b8079d17a0680889d93d9ee4b7404c
Michael Jardinico <michael.jardinico@csun.edu>
2023-11-16 11:31:00 -0800

    add list_employee_hiredate.sql schema

---
+++ EmployeeSQL/list_employee_hiredate.sql
@@ -0,0 +1,6 @@
+-- List the first name, last name, and hire date for the employees 
+-- who were hired in 1986 (2 points)
+
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE EXTRACT(YEAR FROM hire_date) = 1986;
