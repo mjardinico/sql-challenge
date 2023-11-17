113d21910b0da22dc1412ca84a8f995e56eafe92
Michael Jardinico <michael.jardinico@csun.edu>
2023-11-16 23:37:28 -0800

    add QuickDBD-table_schemas.sql

---
+++ QuickDBD-table_schemas.sql
@@ -0,0 +1,39 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "employees" (
+    "emp_no" INT   NOT NULL,
+    "emp_title" VARCHAR(10)   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR(30)   NOT NULL,
+    "last_name" VARCHAR(30)   NOT NULL,
+    "sex" VARCHAR(10)   NOT NULL,
+    "hire_date" DATE   NOT NULL
+);
+
+CREATE TABLE "departments" (
+    "dept_no" VARCHAR(10)   NOT NULL,
+    "dept_name" VARCHAR(50)   NOT NULL
+);
+
+CREATE TABLE "dept_emp" (
+    "dept_no" VARCHAR(10)   NOT NULL,
+    "emp_no" INT   NOT NULL
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_no" VARCHAR(10)   NOT NULL,
+    "emp_no" INT   NOT NULL
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" INT   NOT NULL,
+    "salary" INT   NOT NULL
+);
+
+CREATE TABLE "titles" (
+    "title_id" VARCHAR(10)   NOT NULL,
+    "title" VARCHAR(50)   NOT NULL
+);
+
